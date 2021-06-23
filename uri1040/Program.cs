30a7943737921859ada66c6e7862383bc58f4605
Andrei Souza <andreycode@hotmail.com>
2021-06-23 12:57:19 -0300

    Passa tempo brincando com uri onile

---
+++ uri1040/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int par = 0;
+            int impar = 0;
+            int positivo = 0;
+            int negativo = 0;
+
+            num = int.Parse(Console.ReadLine());
+            if (num < 0)
+            {
+                negativo++;
+            }
+            else if(num > 0)
+            {
+                positivo++;
+            }
+            if(num %2 == 0)
+            {
+                par++;
+            }
+            else
+            {
+                impar++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num < 0)
+            {
+                negativo++;
+            }
+            else if (num > 0)
+            {
+                positivo++;
+            }
+            if (num % 2 == 0)
+            {
+                par++;
+            }
+            else
+            {
+                impar++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num < 0)
+            {
+                negativo++;
+            }
+            else if (num > 0)
+            {
+                positivo++;
+            }
+            if (num % 2 == 0)
+            {
+                par++;
+            }
+            else
+            {
+                impar++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num < 0)
+            {
+                negativo++;
+            }
+            else if (num > 0)
+            {
+                positivo++;
+            }
+            if (num % 2 == 0)
+            {
+                par++;
+            }
+            else
+            {
+                impar++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num < 0)
+            {
+                negativo++;
+            }
+            else if (num > 0)
+            {
+                positivo++;
+            }
+             if (num % 2 == 0)
+            {
+                par++;
+            }
+            else
+            {
+                impar++;
+            }
+
+            Console.WriteLine(par + " valor(es) par(es)");
+            Console.WriteLine(impar + " valor(es) impar(es)");
+            Console.WriteLine(positivo + " valor(es) positivo(a)");
+            Console.WriteLine(negativo + " valor(es) negativo(a)");
+
+        } 
+    }
+}
